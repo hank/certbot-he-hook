0f19679a96efab12f6707cc4f60897e4af27f92b
hank <hank@jointsecurityarea.org>
2018-09-21 22:18:12 +0000

    Revamping update script a little

--- update-example.sh
+++ update-example.sh
@@ -1,12 +1,17 @@
 export HE_USERNAME=user
 export HE_PASSWORD=pw
-export HE_ZONE=some.domain
+export HE_ZONE=somezone
+export HE_SUBDOMAIN=octopi
 
 certbot certonly \
-    --domain pi.some.domain \
-    --email root@some.domain \
+    --domain ${HE_SUBDOMAIN}.${HE_ZONE} \
+    --email root@${HE_ZONE} \
     --preferred-challenges dns \
     --manual \
     --manual-auth-hook "/opt/certbot-he-hook.py"  \
     --manual-cleanup-hook "/opt/certbot-he-hook.py"  \
     --manual-public-ip-logging-ok
+
+pushd /etc/letsencrypt/live/${HE_SUBDOMAIN}.${HE_ZONE}/
+cat fullchain.pem privkey.pem > certandkey.pem
+popd
